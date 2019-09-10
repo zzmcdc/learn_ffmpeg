8a6eb03dd9973d3e41ff85269c4e069a9f0a3e38
ZHAOZHIMING <zhaozhiming@kingsoft.com>
2019-09-10 15:02:00 +0800

    python binding is ok

--- gpu_decode/VideoCapture.py
+++ gpu_decode/VideoCapture.py
@@ -1,14 +1,14 @@
 import numpy as np
-from video import VideoCapture as video
+from video import VideoCapture as Video
 
 class VideoCapture(object):
-  def __init__(file_name, gpu_id, resize=(0, 0)):
-    self.video = video(file_name, gpu_id, resize)
+  def __init__(self,file_name, gpu_id, resize=(0, 0)):
+    self.video = Video(file_name, gpu_id, resize)
     self.is_stop = False
 
-  def read():
-    img = np.array(video.read(), copy=False)
+  def read(self):
+    img = np.array(self.video.read(), copy=False)
     self.is_stop = self.video.is_stop
-    if not self.is_stop:
+    if self.is_stop:
       return None
     return self.is_stop, img
