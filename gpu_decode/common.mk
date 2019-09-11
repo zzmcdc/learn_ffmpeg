ab0a41fdcdbde27b53b2fd3dfdf93f27eaa08862
ZHAOZHIMING <zhaozhiming@kingsoft.com>
2019-09-11 19:51:12 +0800

    nop

--- gpu_decode/common.mk
+++ gpu_decode/common.mk
@@ -15,17 +15,13 @@ GCC ?= g++
 
 CCFLAGS := -std=c++11
 
-# Debug build flags
-ifeq ($(dbg),1)
-    CCFLAGS += -g
-endif
 
 CUDA_PATH ?= /usr/local/cuda
 
 # Link applications against stub libraries provided in the SDKs.
 LDFLAGS := -L$(CUDA_PATH)/lib64/stubs
 LDFLAGS += -LLib/linux/stubs/$(shell uname -p)
-LDFLAGS += -ldl -lcuda -fopenmp
+LDFLAGS += -ldl -lcuda 
 
 NVCC ?= $(CUDA_PATH)/bin/nvcc
 
